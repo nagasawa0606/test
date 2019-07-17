22ad0e14c7fa139b516c0a77cca0fe4db3774e84
nagasawa0606 <gucci.tennismath@gmail.com>
2019-07-17 17:49:05 +0900

    7/17

---
+++ human/src/jp/co/intenous/action/HumanAddress.java
@@ -0,0 +1,11 @@
+package jp.co.intenous.action;
+
+public class HumanAddress {
+	
+	public String address;
+	public String getAddress(){
+		address="東京";
+		return address;
+	}
+
+}
---
+++ human/src/jp/co/intenous/action/HumanAge.java
@@ -0,0 +1,11 @@
+package jp.co.intenous.action;
+
+public class HumanAge {
+	
+	public int age;
+	public int getAge(){
+		age=25;
+		return age;
+	}
+
+}
